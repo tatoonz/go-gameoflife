2e81e5794bcf3c62b461431e5002726b59806a14
TaToonz <peerapath.t@gmail.com>
2019-10-05 11:54:35 +0700

    use time.tick for decrease game render speed

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
@@ -8,8 +10,6 @@ import (
 )
 
 var (
-	rows     = 5
-	columns  = 5
 	cellSize = 20
 )
 
@@ -18,18 +18,19 @@ func main() {
 }
 
 func run() {
-	uni := newUniverse(5, 5)
+	uni := newUniverse(6, 6)
 	uni.cells = [][]bool{
-		{false, false, false, false, false},
-		{false, false, false, false, false},
-		{false, true, true, true, false},
-		{false, false, false, false, false},
-		{false, false, false, false, false},
+		{false, false, false, false, false, false},
+		{false, true, false, true, false, false},
+		{false, false, true, true, false, false},
+		{false, false, true, false, false, false},
+		{false, false, false, false, false, false},
+		{false, false, false, false, false, false},
 	}
 
 	win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
 		Title:  "Game Of Life",
-		Bounds: pixel.R(0, 0, float64(columns*cellSize), float64(columns*cellSize)),
+		Bounds: pixel.R(0, 0, float64(uni.cols*cellSize), float64(uni.rows*cellSize)),
 		VSync:  true,
 	})
 
@@ -40,27 +41,32 @@ func run() {
 	win.Clear(colornames.Whitesmoke)
 
 	imd := imdraw.New(nil)
+	tick := time.Tick(500 * time.Millisecond)
 	for !win.Closed() {
-		imd.Clear()
+		select {
+		case <-tick:
+			imd.Clear()
 
-		for x := 0; x < columns; x++ {
-			for y := 0; y < rows; y++ {
-				imd.Color = colornames.Whitesmoke
+			for x := 0; x < uni.cols; x++ {
+				for y := 0; y < uni.rows; y++ {
+					imd.Color = colornames.Whitesmoke
 
-				if uni.cellAlive(x, y) {
-					imd.Color = colornames.Black
+					if uni.cellAlive(x, y) {
+						imd.Color = colornames.Black
+					}
+
+					imd.Push(
+						pixel.V(float64(x*cellSize), float64((uni.rows-y-1)*cellSize)),
+						pixel.V(float64((x+1)*cellSize), float64((uni.rows-y)*cellSize)),
+					)
+					imd.Rectangle(0)
 				}
-
-				imd.Push(
-					pixel.V(float64(x*cellSize), float64((rows-y-1)*cellSize)),
-					pixel.V(float64((x+1)*cellSize), float64((rows-y)*cellSize)),
-				)
-				imd.Rectangle(0)
 			}
-		}
-		uni = uni.next()
 
-		imd.Draw(win)
+			imd.Draw(win)
+			uni = uni.next()
+		}
+
 		win.Update()
 	}
 }

ecf9cc6bad84815d6ffbf6e1a4e58e20b915c45c
TaToonz <peerapath.t@gmail.com>
2019-10-05 10:15:38 +0700

    extract count neighbour lives to another function

--- universe.go
+++ universe.go
@@ -11,50 +11,57 @@ func (u *universe) cellAlive(x, y int) bool {
 }
 
 func (u *universe) nextLifeOfCell(x, y int) bool {
-	liveNeighBours := 0
-	// top left
-	if u.cellAlive(x-1, y-1) {
-		liveNeighBours++
-	}
-
-	// left
-	if u.cellAlive(x-1, y) {
-		liveNeighBours++
-	}
-
-	// bottom left
-	if u.cellAlive(x-1, y+1) {
-		liveNeighBours++
-	}
-
-	// top
-	if u.cellAlive(x, y-1) {
-		liveNeighBours++
-	}
-
-	// bottom
-	if u.cellAlive(x, y+1) {
-		liveNeighBours++
-	}
-
-	// top right
-	if u.cellAlive(x+1, y-1) {
-		liveNeighBours++
-	}
-
-	// right
-	if u.cellAlive(x+1, y) {
-		liveNeighBours++
-	}
-
-	// bottom right
-	if u.cellAlive(x+1, y+1) {
-		liveNeighBours++
-	}
+	liveNeighBours := u.countNeighbourLivesOfCell(x, y)
 
 	if liveNeighBours == 2 || liveNeighBours == 3 {
 		return true
 	}
 
 	return false
+}
+
+func (u *universe) countNeighbourLivesOfCell(x, y int) int {
+	result := 0
+
+	// top left
+	if u.cellAlive(x-1, y-1) {
+		result++
+	}
+
+	// left
+	if u.cellAlive(x-1, y) {
+		result++
+	}
+
+	// bottom left
+	if u.cellAlive(x-1, y+1) {
+		result++
+	}
+
+	// top
+	if u.cellAlive(x, y-1) {
+		result++
+	}
+
+	// bottom
+	if u.cellAlive(x, y+1) {
+		result++
+	}
+
+	// top right
+	if u.cellAlive(x+1, y-1) {
+		result++
+	}
+
+	// right
+	if u.cellAlive(x+1, y) {
+		result++
+	}
+
+	// bottom right
+	if u.cellAlive(x+1, y+1) {
+		result++
+	}
+
+	return result
 }

66fa1d60369972513f463d6a4bba13f305e71875
TaToonz <peerapath.t@gmail.com>
2019-10-05 10:12:19 +0700

    add case cell should be alive when has 2-3 live neighbour

--- universe.go
+++ universe.go
@@ -11,5 +11,50 @@ func (u *universe) cellAlive(x, y int) bool {
 }
 
 func (u *universe) nextLifeOfCell(x, y int) bool {
+	liveNeighBours := 0
+	// top left
+	if u.cellAlive(x-1, y-1) {
+		liveNeighBours++
+	}
+
+	// left
+	if u.cellAlive(x-1, y) {
+		liveNeighBours++
+	}
+
+	// bottom left
+	if u.cellAlive(x-1, y+1) {
+		liveNeighBours++
+	}
+
+	// top
+	if u.cellAlive(x, y-1) {
+		liveNeighBours++
+	}
+
+	// bottom
+	if u.cellAlive(x, y+1) {
+		liveNeighBours++
+	}
+
+	// top right
+	if u.cellAlive(x+1, y-1) {
+		liveNeighBours++
+	}
+
+	// right
+	if u.cellAlive(x+1, y) {
+		liveNeighBours++
+	}
+
+	// bottom right
+	if u.cellAlive(x+1, y+1) {
+		liveNeighBours++
+	}
+
+	if liveNeighBours == 2 || liveNeighBours == 3 {
+		return true
+	}
+
 	return false
 }
--- universe_test.go
+++ universe_test.go
@@ -17,3 +17,29 @@ func TestNextLifeOfCell_ShouldReturnFalse_WhenNeighbourLivesLessThanTwo(t *testi
 
 	assert.False(t, u.nextLifeOfCell(1, 1))
 }
+
+func TestNextLifeOfCell_ShouldStillAlive_WhenCellIsAlive(t *testing.T) {
+	t.Run("AndHas2LiveNeighbour", func(t *testing.T) {
+		u := &universe{
+			cells: [][]bool{
+				{true, false, false},
+				{false, true, true},
+				{false, false, false},
+			},
+		}
+
+		assert.True(t, u.nextLifeOfCell(1, 1))
+	})
+
+	t.Run("AndHas3LiveNeighbour", func(t *testing.T) {
+		u := &universe{
+			cells: [][]bool{
+				{true, false, true},
+				{false, true, true},
+				{false, false, false},
+			},
+		}
+
+		assert.True(t, u.nextLifeOfCell(1, 1))
+	})
+}
